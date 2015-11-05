5e2fb23a9cbaae2381af495df4cd3652f735d72f
EvaSokol <mask@pisem.net>
2015-11-05 15:24:33 +0200

    Homework 1: assert added.

--- src/test/java/HwFirst.java
+++ src/test/java/HwFirst.java
@@ -2,6 +2,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
@@ -12,15 +14,18 @@ public class HwFirst {
         protected static WebDriver driver;
 
         @Test
-        public void Lesson1(){
+        public void Lesson1() throws InterruptedException {
             String baseUrl = "http://localhost/wordpress/wp-login.php";
             driver = new FirefoxDriver();
             driver.navigate().to(baseUrl);
             driver.findElement(By.id("user_login")).clear();
-            driver.findElement(By.id("user_login")).sendKeys("Eva");
+            driver.findElement(By.id("user_login")).sendKeys("user1");
             driver.findElement(By.id("user_pass")).clear();
-            driver.findElement(By.id("user_pass")).sendKeys("eva123");
+            driver.findElement(By.id("user_pass")).sendKeys("user1");
             driver.findElement(By.id("wp-submit")).click();
+
+            Assert.assertTrue(driver.findElement(By.id("profile-page")).isDisplayed());
+
             driver.close();
             driver.quit();
             }
